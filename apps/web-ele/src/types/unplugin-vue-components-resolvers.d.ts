2dc18bbbc34dae7eddf39e307f7e3d46bc67f44b
tangxin <xintang@deepglint.com>
2026-03-31 20:28:38 +0800

    feat: ai coding

---
+++ apps/web-ele/src/types/unplugin-vue-components-resolvers.d.ts
@@ -0,0 +1,6 @@
+declare module 'unplugin-vue-components/resolvers' {
+  import type { ComponentResolver } from 'unplugin-vue-components';
+
+  export function ElementPlusResolver(): ComponentResolver;
+}
+
